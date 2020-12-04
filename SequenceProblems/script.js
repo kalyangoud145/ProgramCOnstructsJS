bdae1a8d4332b20837d4ab0b0ab5622fde803f52
kalyangoud145 <kalyangoud145@gmail.com>
2020-12-04 13:03:24 +0530

    [Kalyan] added: logic to get sum, average of 5 two digit random numbers

--- SequenceProblems/script.js
+++ SequenceProblems/script.js
@@ -7,3 +7,10 @@ console.log("Dice Number: " + diceRoll + "\n");
 // 3. Sum of two dice numbers
 let twoDiceSum = Math.floor(Math.random() * 6 + 1) + Math.floor(Math.random() * 6 + 1);
 console.log("Sum of two dice numbers: " + twoDiceSum + "\n");
+// 4. Generate 5 random 2-digit numbers and find their sum and average value;
+let sum = 0;
+for (let i = 0; i < 5; i++) {
+	let x = Math.floor(Math.random() * 90 + 10);
+	console.log("number " + (i + 1) + " is: " + x);
+	sum += x;
+}
