76bd454ce930622e86e3783725a6c4a797365a52
kalyangoud145 <kalyangoud145@gmail.com>
2020-12-04 13:01:05 +0530

    [Kalyan] added: logic to retrive two dice roll sum

--- SequenceProblems/script.js
+++ SequenceProblems/script.js
@@ -1,6 +1,9 @@
 // 1. Random function to get single digit
 let randomNumber = Math.floor(Math.random() * 10);
 console.log("Single Digit Random Number: " + randomNumber + "\n");
-//2. Generate dice number between 1 to 6
-let b = Math.floor(Math.random() * 6 + 1);
-console.log("Dice Number: " + b + "\n");
+// 2. Generate dice number between 1 to 6
+let diceRoll = Math.floor(Math.random() * 6 + 1);
+console.log("Dice Number: " + diceRoll + "\n");
+// 3. Sum of two dice numbers
+let twoDiceSum = Math.floor(Math.random() * 6 + 1) + Math.floor(Math.random() * 6 + 1);
+console.log("Sum of two dice numbers: " + twoDiceSum + "\n");
