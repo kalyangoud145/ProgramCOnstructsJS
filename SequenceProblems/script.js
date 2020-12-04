9846f85a2dbda3fff4066ec4e3de236664ce3c18
kalyangoud145 <kalyangoud145@gmail.com>
2020-12-04 12:51:22 +0530

    [Kalyan] added: logic to generate number from 1 to 6

--- SequenceProblems/script.js
+++ SequenceProblems/script.js
@@ -1,3 +1,6 @@
 // 1. Random function to get single digit
 let randomNumber = Math.floor(Math.random() * 10);
 console.log("Single Digit Random Number: " + randomNumber + "\n");
+//2. Generate dice number between 1 to 6
+let b = Math.floor(Math.random() * 6 + 1);
+console.log("Dice Number: " + b + "\n");
