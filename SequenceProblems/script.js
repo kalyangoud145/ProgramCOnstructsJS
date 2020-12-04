f692e6fa1ab4e30917405767b75727e618b1fdaf
kalyangoud145 <kalyangoud145@gmail.com>
2020-12-04 13:27:07 +0530

    [Kalyan] added: logic for unit conversion problems

--- SequenceProblems/script.js
+++ SequenceProblems/script.js
@@ -14,3 +14,15 @@ for (let i = 0; i < 5; i++) {
 	console.log("number " + (i + 1) + " is: " + x);
 	sum += x;
 }
+console.log("Sum of five random 2-digit numbers is: " + sum);
+console.log("Avg of five random 2-digit numbers is: " + (sum / 5) + "\n");
+
+// 5. Unit conversions
+const ONE_INCH_IN_FEET = 1 / 12;
+console.log("42 inches in : " + ONE_INCH_IN_FEET * 42 + " feets\n");
+
+let area_Of_One_Plot = (60 * 40 * 100/ 328);
+console.log("Area of 60feet X 40feet in meters is: " + area_Of_One_Plot);
+
+let area_Of_25_Plots = (area_Of_One_Plot * 247 *25/ 1000000) ;
+console.log("Area of 25 such plots: " + 25 * area_Of_25_Plots);
