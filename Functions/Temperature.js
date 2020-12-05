c645be2dbc4ad5ce6e6ea9fc02048d64e2d6a6d7
kalyangoud145 <kalyangoud145@gmail.com>
2020-12-05 18:53:29 +0530

    [Kalyan] added: program to convert  temperature  degF to degC and vice versa

---
+++ Functions/Temperature.js
@@ -0,0 +1,26 @@
+const readline = require('readline-sync');
+let str = readline.question("Enter y for degF to degC conversion, n for vice-versa: ");
+str = str.toLowerCase();
+let temp_str = readline.question("Enter temperature: ");
+let temp = parseInt(temp_str);
+// Converts farenheits to degree centigrades temperature
+function FtoDegC(temp) {
+	if (temp < 32 || temp > 212) {
+		console.log("Invalid input!");
+		return;
+	}
+	console.log("temp is degC is: " + ((temp - 32) * (5 / 9)));
+}
+// Converts centigrades to farenheits temperature
+function CtoDegF(temp) {
+	if (temp < 0 || temp > 100) {
+		console.log("Invalid input!");
+		return;
+	}
+	console.log("temp in degF is: " + ((temp * 9 / 5) + 32));
+}
+if (str == "y") {
+	FtoDegC(temp);
+} else {
+	CtoDegF(temp);
+}
